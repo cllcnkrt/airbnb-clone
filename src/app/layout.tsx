668862c0ad95a0c13fc433be0d60671f5c3f0690
cllcnkrt <cllcnkrt@outlook.com>
2024-09-09 22:59:07 +0200

    fix: some fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             <body className={nunito.className}>
                 <ToasterProvider />
                 <RentModal />
-                <LoginModal />
+                {/* <LoginModal /> */}
                 <RegisterModal />
                 <Navbar currentUser={currentUser} />
                 {children}
